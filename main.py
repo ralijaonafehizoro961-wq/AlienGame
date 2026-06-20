783c61dbe8b65de998828c98274662adb8bb1250
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-20 14:52:45 +0300

    refractor: class separation in each file

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 import pygame
+from game import Game
 pygame.init()
-
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.health = 100
-        self.max_health = 100
-        self.attack = 10
-        self.velocity = 5
-        self.image = pygame.image.load('assets/player.png')
-        self.rect = self.image.get_rect()
-        self.rect.x  = 400
-        self.rect.y = 500
-
-
 
 pygame.display.set_caption("Alien Game")
 screen = pygame.display.set_mode((1080, 700))
 
 background = pygame.image.load('assets/bg.jpg')
-player = Player()
+game = Game()
 running  = True
 
 while running:
     screen.blit(background,(0, -200))
-    screen.blit(player.image, player.rect)
+    screen.blit(game.player.image, game.player.rect)
 
     pygame.display.flip()
     for event in pygame.event.get():
