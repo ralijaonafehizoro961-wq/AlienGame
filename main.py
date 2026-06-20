9f0df485da48221fa6d0022500f0fc2add289f0d
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-20 15:26:59 +0300

    fix: added fluidity to player movement(2)

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ running  = True
 while running:
     screen.blit(background,(0, -200))
     screen.blit(game.player.image, game.player.rect)
-    if game.pressed.get(pygame.K_RIGHT):
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
         game.player.move_right()
-    elif game.pressed.get(pygame.K_LEFT):
+    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
             game.player.move_left()
+
+    print(game.player.rect.x)
 
     pygame.display.flip()
     for event in pygame.event.get():
