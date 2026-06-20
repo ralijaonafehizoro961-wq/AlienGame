ce1fb47c11da4e50f023d88ea541f42ea8b12628
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-06-20 10:55:45 +0300

    feat:player added

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import pygame
 pygame.init()
 
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load('assets/player.png')
+        self.rect = self.image.get_rect()
+        self.rect.x  = 400
+        self.rect.y = 500
+
+
+
 pygame.display.set_caption("Alien Game")
 screen = pygame.display.set_mode((1080, 700))
 
 background = pygame.image.load('assets/bg.jpg')
+player = Player()
 running  = True
 
 while running:
     screen.blit(background,(0, -200))
+    screen.blit(player.image, player.rect)
+
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
